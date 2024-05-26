19dbe407842727c4b7d7f544120056fbbdade260
Miheret Girmachew <124549518+Miheret-Girmachew@users.noreply.github.com>
2024-05-26 22:47:50 +0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html")
+def register(request):
+    return render(request, "registration/register.html", {"form": form})
