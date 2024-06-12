e4f3fdc25696aef093c18474b8e003ca5903036a
fitiha <fitihanegest1995@gmail.com>
2024-06-12 22:57:42 +0300

    added a task feature for the calendar

--- core/MI/calendar/urls.py
+++ core/MI/calendar/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('event/new/', views.event_create, name='event_create'),
     path('event/<int:pk>/edit/', views.event_update, name='event_update'),
     path('event/<int:pk>/delete/', views.event_delete, name='event_delete'),
+    path('task/<int:pk>/', views.task_detail, name='task_detail'),
+    path('task/new/', views.task_create, name='task_create'),
+    path('task/<int:pk>/edit/', views.task_update, name='task_update'),
+    path('task/<int:pk>/delete/', views.task_delete, name='task_delete'),
 ]
