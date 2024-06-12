0a16ed054bfc87472c95ebec76ca797e56a92bea
fitiha <fitihanegst1995@gmail.com>
2024-06-12 22:35:53 +0300

    some changes made in the MI

--- core/MI/calendar/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Event
-
-class EventForm(forms.ModelForm):
-    class Meta:
-        model = Event
-        fields = ['title', 'description', 'start_time', 'end_time']
